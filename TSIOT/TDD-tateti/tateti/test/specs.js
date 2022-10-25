943c5a23dc9980a9f334044f093ae7a876708a15
Ricky <losadaricardo59@gmail.com>
2022-10-24 21:07:00 -0300

    TDD-tateti primer commit

---
+++ TSIOT/TDD-tateti/tateti/test/specs.js
@@ -0,0 +1,8 @@
+let chai = require("chai");
+let should = chai.should();
+
+describe("Juego del Tateti", function() {
+    it("Se empieza el juego nuevo", () => {
+        chai.assert.fail("empezamos")
+    })
+})
